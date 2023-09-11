57d135b6ac382b05406f19eea9e6e3ac3658f90b
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-09-11 21:37:26 +0200

    created files for tecnologies

--- routes/web.php
+++ routes/web.php
@@ -34,3 +34,5 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__ . '/auth.php';
+
+Route::resource('admin/technologies', App\Http\Controllers\Admin\TechnologiesController::class, ['as' => 'admin']);
